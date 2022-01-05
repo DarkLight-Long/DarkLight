34e8fb3429d96bc9d5d82900233469a1431809f6
Dark Light <48716334+DarkLight-Long@users.noreply.github.com>
2022-01-05 15:53:45 +0800

    webpack注释添加

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
 //对 tsconfig.json的补充 
 module.exports = {
+    // 静态文件加载路径
     publicPath: "./",
+    // 编译后文件放置位置
     outputDir: "dist",
+    // 编译后js,css文件放置到/dist/static目录下，方便nginx访问
     assetsDir: "static",
     productionSourceMap: false
 }
